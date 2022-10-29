0c1f3804d1a836506b1ce44aa975c73d4aa9ce55
Olumide Bambe <loyalsfc@gmail.com>
2022-10-29 04:27:41 +0100

    removed unused files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      color:{
+        "grey-200": "#EAECF0",
+      }
+    },
+  },
+  plugins: [],
+}
